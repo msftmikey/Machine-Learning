8896af3286ff34416b5fa2508532d24e6e02a102
Miguel Mancera <miguelmancera997@gmail.com>
2024-02-14 10:50:33 -0800

    Added project files

---
+++ model.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# Read the dataset into a DataFrame
+df = pd.read_csv('dataset.csv')
+
+# Filter the DataFrame to include rows with relevant indicators
+relevant_indicators = ["Deaths from suicide, alcohol, drugs"]
+filtered_df = df[df["Indicator"].isin(relevant_indicators)]
+
+more_indicators = ["Homicides"]
+more_filtered_df = df[df["Indicator"].isin(more_indicators)]
+
+print(filtered_df[["Indicator", "Value"]].head())
+print(more_filtered_df[["Indicator", "Value"]].head())
